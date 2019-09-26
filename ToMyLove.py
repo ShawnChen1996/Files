067f4763f000e8f7fc6d5faca009ea613dd8ed27
ShawnChen1996 <shawnchen1996@outlook.com>
2019-09-26 20:48:50 +0800

    Create ToMyLove.py

---
+++ ToMyLove.py
@@ -0,0 +1,7 @@
+
+
+print("Hello World!")
+s = "ShawnLovesYouSooooMuch"
+print('\n'.join([''.join([(s[(x-y)%len(s)]if((x*0.05)**2+(y*0.1)**2-1)**3-(x*0.05)**2*(y*0.1)**3<=0 else' ')for x in range(-30,30)])for y in range(15,-15,-1)]))
+
+print('小青青...爱你的形状送给你')
